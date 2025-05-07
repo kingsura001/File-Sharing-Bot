1fe1f6df05ec9b83b8045ae65162603541feb95a
kingsura001 <kingsura001@gmail.com>
2025-05-07 13:49:47 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"Hlw",
+            text = f"<b>○ Hlw ",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
